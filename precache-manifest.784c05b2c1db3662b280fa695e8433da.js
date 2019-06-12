faff2e04610070dfed2e865e990bd6cc777b26f8
Dimitri <dimitri@student-net-hix-1858.ethz.ch>
2019-06-12 19:49:03 +0200

    deploy

---
+++ precache-manifest.784c05b2c1db3662b280fa695e8433da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c18bfa08a790a15f4ac6",
+    "url": "/pwa/js/about.882a498e.js"
+  },
+  {
+    "revision": "e4ab1ff4cf3807dda768",
+    "url": "/pwa/css/app.48f5c24c.css"
+  },
+  {
+    "revision": "e4ab1ff4cf3807dda768",
+    "url": "/pwa/js/app.1e4a5599.js"
+  },
+  {
+    "revision": "a634ee8ce93f91bc9fb1",
+    "url": "/pwa/js/chunk-vendors.94deeec4.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3ac37ca0a9407411e98e2ed833a3e979",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa/robots.txt"
+  }
+];
